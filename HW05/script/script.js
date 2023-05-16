a43e0a40e54e7cb932d000fb54f46a72ecc55699
SemenVailega <semenvasilega@gmail.com>
2023-05-16 19:13:53 +0300

    Home work 5 - second edition :)

--- HW05/script/script.js
+++ HW05/script/script.js
@@ -25,10 +25,10 @@ function CarFabric (yearOfManufacture, model, speedLimit){
     this.speedLimit = speedLimit;    
 }
 CarFabric.prototype.carInfo = function () {
-        function yearFunc (object){
-        let year = data.getFullYear();        
-        console.log(`Your car is ${year} years old`);
-    }
+    //     function yearFunc (object){
+    //     let year = data.getFullYear();        
+    //     console.log(`Your car is ${year} years old`);
+    // }    
     console.log(`Year of manufacture - ${this.yearOfManufacture}, model - ${this.model}, speed limit is - ${this.speedLimit}`);
 }
 
@@ -40,8 +40,13 @@ console.log('Task 3');
 // 3) На прототипі конструктора зробіть функцію carInfo яка 
 // вираховує вік машини використовуючи new Date
 const data = new Date();
-// let year = Number(data.getFullYear());
-// console.log(year);
 
-// porshe.carInfo.yearFunc();
-porshe.carInfo.yearFunc(data);
+function yearManufacture(object,data){
+    let year = Number(data.getFullYear());
+    let yearMan = object;    
+    console.log(`Today is ${year}, year manufacture - ${yearMan}.Car in privios task is - ${year - yearMan} years old`);
+        
+}
+yearManufacture(porshe.yearOfManufacture,data);
+// console.log(`${porshe.carInfo()} + this car is - ${year} years old`);
+
